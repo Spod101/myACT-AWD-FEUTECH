960e411298a45b3dcd6afdb5d42f60da44a6566c
Dalet <104555136+Spod101@users.noreply.github.com>
2024-03-03 23:03:01 +0800

    Delete ACT2 directory

--- ACT2/scripts/script.js
+++
@@ -1,7 +0,0 @@
-const letter = document.querySelectorAll('#_letters #_letter');
-
-letter.forEach((letter) => {
-  letter.addEventListener('click', () => {
-    letter.classList.toggle('active');
-  });
-});
